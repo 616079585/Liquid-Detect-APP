c9daad9e90755a8512fff3610208fa8ad8c41117
616079585 <55619400+616079585@users.noreply.github.com>
2019-11-12 18:18:17 +0800

    Code Commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  第一个计算器
+//
+//  Created by xyh on 2019/10/27.
+//  Copyright © 2019 xyh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
